aa7aba48f247ac663f9dbfcfb1829ed46e867cc5
chuckleong21 <chuckleong21@gmail.com>
2024-08-28 14:36:15 +0800

    Add common dependencies

---
+++ R/sample.criteria-package.R
@@ -0,0 +1,14 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom stats binomial
+#' @importFrom stats glm
+#' @importFrom stats logLik
+#' @importFrom stats qchisq
+#' @importFrom stats qnorm
+#' @importFrom stats rbinom
+#' @importFrom stats rnorm
+#' @importFrom utils combn
+## usethis namespace: end
+NULL
