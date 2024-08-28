46a6693995bb1297549285afd7bf5d086ef84752
chuckleong21 <chuckleong21@gmail.com>
2024-08-28 13:48:45 +0800

    Add print S3 method

--- R/helper.r
+++ R/helper.r
@@ -5,18 +5,58 @@ phi_pairs <- function(n_events) {
   x / (x + y)
 }
 
+#' Calculate (cohort) outcome proportions
+#'
+#' @param x The count of events of different outcomes
+#'
+#' @return
+#' A numeric vector of length \eqn{k + C^{k}_{2}} where the first k elements are
+#' \eqn{p_k} and the rest are \eqn{p_{k,r}}
+#' @export
+#'
+#' @examples
+#' props_by_events(c(2557, 186, 176, 467, 120))
 props_by_events <- function(x) {
   p <- c(x / sum(x),
-    colSums(combn(x, 2)) / sum(x))
+         colSums(combn(x, 2)) / sum(x))
   k <- Reduce(c, sapply(seq_len(length(x)-1), function(i) seq(length(x))[-seq_len(i)]))
   r <- rep(seq_along(x)[-length(x)], rev(seq_len(length(x)-1)))
   names(p) <- c(seq_along(x),
                 sprintf("%d_%d", k, r))
-  p
+  list(events = x, p = p)
 }
 
-check_kwargs <- function(kwargs) {
-  arg_match <- lapply(names(kwargs), function(x) try(match.arg(x, c("c_stats", "prev")), silent = TRUE))
-  stopifnot("One or more arguments are not `c_stats`, `prev` when simulation is FALSE" = !all(sapply(arg_match, function(x) inherits(x, "try-error"))))
-  invisible()
+crit <- function(Q, shrinkage = 0.9,
+                 r2_cs = NULL,
+                 max_r2_csapp = NULL,
+                 r2_csadj = NULL,
+                 prev,
+                 sigma = 0.05,
+                 margin_error = 0.05,
+                 ...) {
+  x <- list(...)
+  # criterion 1 -------------------------------------------------------------
+
+  if("r2_csadjkr" %in% names(x)) {
+    crit1 <- ceiling(max(Q / ((shrinkage-1)*log(1-x$r2_csadjkr/shrinkage)) * 1/x$pkr))
+  } else {
+    crit1 <- ceiling(Q/((shrinkage - 1)*log(1-r2_csadj/shrinkage)))
+  }
+
+  # criterion 2 -------------------------------------------------------------
+
+  if("r2_csadjkr" %in% names(x)) {
+    crit2 <- ceiling((x$k-1) * Q / ((r2_csadj/(r2_csadj + sigma*max_r2_csapp)-1)*log(1-r2_csadj-sigma*max_r2_csapp)))
+  } else {
+    crit2 <- ceiling(Q/((round(r2_csadj / (r2_csadj + sigma * max_r2_csapp), 3) - 1)*log(1-r2_csadj/(round(r2_csadj / (r2_csadj + sigma * max_r2_csapp), 3)))))
+  }
+  # criterion 3 -------------------------------------------------------------
+
+  if("pkr" %in% names(x)) {
+    crit3 <- ceiling(max(qchisq(0.05/x$k, 1, lower.tail = FALSE)*x$pk*(1-x$pk) / sigma^2))
+  } else {
+    crit3 <- ceiling((1.96/margin_error)^2*prev*(1-prev))
+  }
+
+  c(crit1, crit2, crit3)
 }
--- R/pmsamplesize.r
+++ R/pmsamplesize.r
@@ -1,27 +1,170 @@
+#' @exportS3Method base::print
+print.pmsample <- function(x, ...) {
+  message(paste("NB: Assuming", x$sigma, "acceptable difference in apparent & adjusted R-squared"))
+  message(paste("NB: Assuming", x$margin_error, "margin of error in estimation of intercept"))
+  message(paste("NB: Events per Predictor Parameter assumes prevalance", round(x$prev, 3), "\n"))
+  print(x$criteria)
+  cat(sprintf("Minimum sample size required for new model development based on user inputs = %.0f,
+ with %.0f events (assuming an outcome prevalence = %.3g) and an EPP = %.3g",
+              x$final$sample_size,
+              x$final$sample_size * x$prev,
+              x$prev, x$final$EPP))
+}
+
+#' @exportS3Method base::print
+print.pmsample_multi <- function(x, ...) {
+  message(paste("NB: Assuming", x$sigma, "acceptable difference in apparent & adjusted R-squared"))
+  message(paste("NB: Assuming", x$margin_error, "margin of error in estimation of intercept"))
+  print(x$criteria)
+}
+
+pmsamplesize_binary <- function(Q,
+                                p,
+                                r2_cs,
+                                shrinkage = 0.9,
+                                r2_nagelkerke = 0.15,
+                                auc,
+                                prev,
+                                sigma = 0.05,
+                                margin_error = 0.05) {
+  if(is.null(r2_cs) & missing(p)) {
+    r2 <- approximate_R2(k = 2, auc = auc, prev = prev)
+    max_r2_csapp <- r2[["R2.nagelkerke"]]
+    r2_csadj <- r2[["R2.coxsnell"]]
+
+    crits <- crit(Q = Q,
+                  max_r2_csapp = max_r2_csapp,
+                  r2_csadj = r2_csadj,
+                  prev = prev)
+  }
+
+  if(is.null(r2_cs) & !missing(p)) {
+    k <- 2; events <- p$events; pr <- p$p[-which(p$p == 1)]
+    names(pr) <- NULL; prev <- ifelse(missing(prev), pr[k], prev)
+    LR <- round(events[k]*log(pr[k])+(sum(events)-events[k])*log(1-pr[k]), 2)
+    max_r2_csapp <- round(1-exp(2*LR/sum(events)), 2)
+    r2_csapp <- r2_csadj <- r2_nagelkerke * max_r2_csapp
+
+    crits <- crit(Q = Q,
+                  max_r2_csapp = max_r2_csapp,
+                  r2_csadj = r2_csadj,
+                  prev = prev)
+  }
+
+  if(!is.null(r2_cs)) {
+    max_r2_csapp <- r2_cs; r2_csadj <- r2_nagelkerke * max_r2_csapp
+    crits <- crit(Q = Q,
+                  max_r2_csapp = max_r2_csapp,
+                  r2_csadj = r2_csadj,
+                  prev = prev)
+  }
+
+  res <- data.frame(sample_size = crits,
+                    shrinkage = c(shrinkage, rep(r2_csadj / (r2_csadj + sigma * max_r2_csapp), 2)),
+                    parameter = Q,
+                    CS_Rsq = r2_csadj,
+                    Max_Rsq = max_r2_csapp,
+                    Nag_Rsq = ifelse(missing(p), max_r2_csapp, r2_nagelkerke),
+                    EPP = crits * prev/Q)
+  res[4, ] <- res[which.max(res$sample_size), ]
+  rownames(res) <- c(paste0("Criteria ", 1:3), "Final")
+
+  x <- list(final = res[4, ],
+            criteria = res,
+            sigma = sigma,
+            margin_error = margin_error,
+            prevalance = prev)
+  class(x) <- c("pmsample")
+  x
+}
+
+pmsamplesize_multi <- function(Q,
+                               k,
+                               p,
+                               r2_cs,
+                               shrinkage = 0.9,
+                               r2_nagelkerke = 0.15,
+                               auc = NULL,
+                               prev = NULL,
+                               sigma = 0.05,
+                               margin_error = 0.05) {
+  events <- p$events; pr <- p$p
+  if(is.null(names(pr))) {
+    kk <- Reduce(c, sapply(seq_len(k-1), function(i) seq(k)[-seq_len(i)]))
+    rr <- rep(seq_len(k)[-k], rev(seq_len(k-1)))
+    names(pr) <- c(seq_len(k),
+                   sprintf("%d_%d", kk, rr))
+  }
+
+  pk <- pr[which(!grepl("_", names(pr)))]
+  pkr <- pr[-which(!grepl("_", names(pr)))]
+
+  if(!is.null(prev)) {
+    prev
+  } else {
+    prev <- phi_pairs(events)
+  }
+  max_r2_csapp <- 1 - (prod(pk^pk)^2)
+  r2_csadj <- r2_nagelkerke * max_r2_csapp
+  if(is.null(auc)) {
+    phis <- phi_pairs(events)
+    max_r2_csappkr <- 1 - (phis^phis*(1-phis)^(1-phis))^2
+    r2_csadjkr <- r2_nagelkerke*max_r2csappkr
+  }
+  r2_csadjkr <- mapply(approximate_R2, k = k, auc = auc, prev = prev)
+  crits <- crit(Q = Q,
+                max_r2_csapp = max_r2_csapp,
+                r2_csadj = r2_csadj,
+                r2_csadjkr = r2_csadjkr,
+                k = k,
+                pk = pk,
+                pkr = pkr)
+  res <- data.frame(sample_size = crits,
+                    shrinkage = c(shrinkage, rep(r2_csadj / (r2_csadj + sigma * max_r2_csapp), 2)),
+                    parameter = Q,
+                    CS_Rsq = r2_csadj,
+                    Max_Rsq = max_r2_csapp,
+                    Nag_Rsq = ifelse(missing(p), max_r2_csapp, r2_nagelkerke)
+                    # EPP = crits * prev/Q
+  )
+  res[4, ] <- res[which.max(res$sample_size), ]
+  rownames(res) <- c(paste0("Criteria ", 1:3), "Final")
+
+  x <- list(final = res[4, ],
+            criteria = res,
+            sigma = sigma,
+            margin_error = margin_error,
+            prevalance = prev)
+  class(x) <- "pmsample_multi"
+  x
+}
+
 #' Sample Criteria Extension for Multinomial Regression
 #'
 #' @importFrom Rdpack reprompt
 #'
 #' @description
-#' Calculate sample criteria basing on multinominal regression. The criteria are
-#' defined in Riley's work.
+#' Calculate sample criteria extended for multinominal regression.
 #'
 #' @details
-#' Additional details...
+#' Sample Criteria for Clinical Prediction Model are based on the work of
+#' (Riley 2018)[https://onlinelibrary.wiley.com/doi/full/10.1002/sim.7992] and
+#' (Calster 2020)[https://onlinelibrary.wiley.com/doi/epdf/10.1002/sim.8806]
 #'
 #' @references
-#' \insertCite{sample.criteria-package}{sample.criteria}
+#' \insertCite{pmsamplesize}{sample.criteria}
 #'
-#' @param k An integer. The number of levels in response variable
 #' @param Q An integer. The number of candidate predictors. See Details
-#' @param props A numeric vector. The proportions of each events
-#' @param n_events A integer vector. The number of each events
+#' @param k An integer. The number of levels in response variable
+#' @param p A numeric vector. Outcome proportion(s)
 #' @param shrinkage A pre-defined shrinkage factor. Default to 0.9
-#' @param r2_cs A numeric vector. Pre-defined Cox-Snell \eqn{R^2}.
+#' @param r2_cs numeric. Cox-Snell \eqn{R^2}.
 #' @param r2_nagelkerke An adjustment for Cox-Snell \eqn{R^2}. Default to 0.15.
-#' @param simulation Either \code{glm} or \code{lrm}. See details.
-#' @param sim_params A list of pairwise C statistics and prevalences
-#' @param sigma A single double. tolerate rate between adjusted \eqn{R^2} and apparent \eqn{R^2}
+#' @param auc numeric. Pairwise C statistics
+#' @param prev numeric. Outcome proportions in categories relative to reference
+#' @param sigma A double. tolerate rate between adjusted \eqn{R^2} and apparent \eqn{R^2}. Default to 0.05
+#' @param margin_error A divergence for margin error specified by Chi-sqaure distribution's confidence level.
+#' Deault to 0.05
 #'
 #' @return An integer
 #' @export
@@ -31,101 +174,23 @@
 #' events <- c(2557, 186, 176, 467, 120)
 #' c_stats <- c(0.85, 0.92, 0.99, 0.95, 0.75, 0.95, 0.87, 0.87, 0.71, 0.82)
 #' prev <- phi_pairs(events)
-#' set.seed(101)
-#' pmsamplesize(k = K, n_events = events,
-#' simulation = "glm", sim_params = list(c_stats = c_stats, prev = prev))
-pmsamplesize <- function(k,
-                         Q = NULL,
-                         props,
-                         n_events,
-                         shrinkage = 0.9,
+#' set.seed(1234)
+#' pmsamplesize(Q = 30, k = 2, auc = 0.81, prev = 0.77)
+pmsamplesize <- function(Q, k, p,
                          r2_cs = NULL,
+                         auc = NULL,
+                         prev,
+                         shrinkage  = 0.9,
                          r2_nagelkerke = 0.15,
-                         simulation = c("glm", "lrm"),
-                         sim_params = NULL,
-                         sigma = 0.05
-) {
-
-
-  # Calculating Q -----------------------------------------------------------
-  # Since Q is available through fractional polynomial method and this
-  # is modeled using the {mrp} package which requires empirical data
-  # that is not currently available
-  # For this work Q=17 will be used though personally I don't that's true
-  # for different k
-  Q <- Q %||% 17
-
-
-  # Calculating p[k] & p[k,r] -----------------------------------------------
-
-  if(missing(props) & missing(n_events)) {
-    stop("One of `props` and `n_events` must be defined")
-  } else if(!missing(props) & !missing(n_events)) {
-    stop("Only one of `props` or `n_events` is used, not both")
-  } else if(!missing(props) & missing(n_events)) {
-    if(length(props) != k + ncol(combn(k, 2))) {
-      stop(sprintf("Expect %d `props` values, but got %d", k + ncol(combn(k, 2)), length(props)))
-    }
-    n_events <- NULL
+                         sigma = 0.05,
+                         margin_error = 0.05) {
+  if(k == 2) {
+    pmsamplesize_binary(Q = Q, p = p, r2_cs = r2_cs,
+                        shrinkage = shrinkage, r2_nagelkerke = r2_nagelkerke, auc = auc, prev = prev,
+                        sigma = sigma, margin_error = margin_error)
   } else {
-    stopifnot(length(n_events) == k)
-    props <- props_by_events(n_events)
+    pmsamplesize_multi(Q = Q, k = k, p = p, r2_cs = r2_cs,
+                       shrinkage = shrinkage, r2_nagelkerke = r2_nagelkerke, auc = auc, prev = prev,
+                       sigma = sigma, margin_error = margin_error)
   }
-
-
-  # Calculating R-square[CS_app] -------------------------------------------
-  if(is.null(names(props))) {
-    kk <- Reduce(c, sapply(seq_len(k-1), function(i) seq(k)[-seq_len(i)]))
-    rr <- rep(seq_len(k)[-k], rev(seq_len(k-1)))
-    names(props) <- c(seq_len(k),
-                  sprintf("%d_%d", kk, rr))
-  }
-  pk <- props[which(!grepl("_", names(props)))]
-  r2_csapp <- 1 - (prod(pk^pk)^2)
-
-  # Calculating R-square[CS_adj] --------------------------------------------
-  r2_csadj <- r2_nagelkerke * r2_csapp
-
-  # Calculating R-square[CS_adj,k,r] ----------------------------------------
-
-  # if pairwise C-statistics are reported
-  if(simulation %in% c("glm", "lrm")) {
-    check_kwargs(sim_params)
-    res <- mapply(approximate_R2,
-                  method = simulation,
-                  auc = sim_params$c_stats,
-                  prev = sim_params$prev)
-
-    if(identical("glm", simulation)) {
-      names(res) <- NULL
-    } else {
-      res <- Reduce(c, res["R2.coxsnell", ])
-    }
-    r2_csadjkr <- res
-  } else {
-    message("`simulation` is either 'glm' or 'lrm'.")
-    warning("argument `sim_args` is ignored for `simulation` argument is invalid")
-    sim_args <- NULL
-  }
-
-
-  # criterion 1 -------------------------------------------------------------
-
-  pkr <- props[which(grepl("_", names(props)))]
-
-  if(!is.null(n_events)) {
-    crit1 <- ceiling(max(Q / ((shrinkage-1)*log(1-r2_csadjkr/shrinkage)) * 1/pkr))
-  } else {
-    crit1 <- ceiling(max(Q / ((shrinkage-1)*log(1-r2_csadjkr/shrinkage)) * 1/pkr))
-  }
-
-# criterion 2 -------------------------------------------------------------
-
-  crit2 <- ceiling((k-1) * Q / ((r2_csadj/(r2_csadj + sigma*r2_csapp)-1)*log(1-r2_csadj-sigma*r2_csapp)))
-
-# criterion 3 -------------------------------------------------------------
-
-  crit3 <- ceiling(max(qchisq(0.05/k, 1, lower.tail = FALSE)*pk*(1-pk) / sigma^2))
-
-  max(c(crit1, crit2, crit3))
 }
