8b40035211348305d987adf0b44d8b598363b048
chuckleong21 <chuckleong21@gmail.com>
2024-08-28 13:49:36 +0800

    Update documentation

--- tests/testthat/test-pmsamplesize.R
+++ tests/testthat/test-pmsamplesize.R
@@ -1,37 +1,18 @@
-# source("R/helper.r")
-# source("R/simulation.R")
-
-K <- 5
 events <- c(2557, 186, 176, 467, 120)
 c_stats <- c(0.85, 0.92, 0.99, 0.95, 0.75, 0.95, 0.87, 0.87, 0.71, 0.82)
-prev <- phi_pairs(events)
 
-test_that("Test Arguments: Only one of props or events should be supplied.", {
-  p <- events / sum(events)
-  expect_error(pmsamplesize(k = K), "One of `props` and `n_events` must be defined")
-  expect_error(
-    pmsamplesize(k = K,
-                 props = props_by_events(events),
-                 n_events = events,
-                 simulation = "glm",
-                 sim_params = list(c_stats = c_stats, prev = prev)),
-    "Only one of `props` or `n_events` is used, not both"
-  )
-  expect_error(pmsamplesize(k = K, props = p),
-               "Expect 15 `props` values, but got 5")
+test_that("Only C statistics & outcome proportion are reported", {
+  set.seed(1234)
+  expect_no_error(pmsamplesize(Q = 30, k = 2, auc = 0.81, prev = 0.77))
 })
 
-test_that("Either specification should return the same result", {
+test_that("When R2_CS is not reported", {
+  expect_no_error(pmsamplesize(Q = 24, k = 2, p = props_by_events(c(138-24, 24)), r2_nagelkerke = 0.48, auc = 0.91))
+})
+
+test_that("Simulation R2 when no r2_csadjk are reported", {
   set.seed(101)
-  p <- props_by_events(events)
-  sample_by_props <- pmsamplesize(k = K, props = p,
-                                  simulation = "glm",
-                                  sim_params = list(c_stats = c_stats, prev = prev))
-  set.seed(101)
-  sample_by_events <- pmsamplesize(k = K, n_events = events,
-                                   simulation = "glm",
-                                   sim_params = list(c_stats = c_stats, prev = prev))
-  expect_identical(sample_by_props,sample_by_events)
+  expect_no_error(pmsamplesize(Q = 17, k = 5, p = props_by_events(events), auc = c_stats, prev = NULL))
 })
 
 
--- tests/testthat/test-simulation.R
+++ tests/testthat/test-simulation.R
@@ -1,21 +1,25 @@
-# source("R/helper.r")
-
 set.seed(101)
 c_stats <- c(0.85, 0.92, 0.99, 0.95, 0.75, 0.95, 0.87, 0.87, 0.71, 0.82)
 events <- c(2557, 186, 176, 467, 120)
 prev <- phi_pairs(events)
-r2cs_adjkr_glm <- mapply(approximate_R2, method = "glm", auc = c_stats, prev = prev)
-r2cs_adjkr_lrm <- mapply(approximate_R2, method = "lrm", auc = c_stats, prev = prev)
-names(r2cs_adjkr_glm) <- NULL
-r2cs_adjkr_lrm <- Reduce(c, r2cs_adjkr_lrm["R2.coxsnell", ])
 
-
-test_that("Method test: glm", {
-  r2cs_adjkr_expected <- c(0.1161076, 0.1793267, 0.4967071, 0.1695518, 0.1851988, 0.4993293, 0.3738221, 0.3280985, 0.1285719, 0.2104207)
-  expect_equal(round(r2cs_adjkr_glm, 3), round(r2cs_adjkr_expected, 3))
+test_that("A legal k should be scalar and an integer that is at least 2", {
+  set.seed(1234)
+  expect_error(approximate_R2(k = 1, auc = c_stats, prev = prev))
+  expect_warning(approximate_R2(k = 2.5, auc = 0.81, prev = 0.77))
+  expect_error(approximate_R2(k = 1, auc= 0.81, prev = 0.77))
 })
 
-test_that("Method test: divergence between two methods all else equal", {
-  expect_failure(expect_equal(r2cs_adjkr_glm, r2cs_adjkr_lrm))
+test_that("Approximate R2 based on binary outcome", {
+  set.seed(1234)
+  r2 <- lapply(approximate_R2(k = 2, auc = 0.81, prev = 0.77), function(x) round(x, 3))
+  expect_equal(r2$R2.nagelkerke, round(0.3183689, 3))
+  expect_equal(r2$R2.coxsnell, round(0.2100957, 3))
 })
 
+test_that("Approximate R2 based on multinomial outcome", {
+  set.seed(101)
+  r2 <- round(mapply(approximate_R2, k = 5, auc = c_stats, prev = prev, USE.NAMES = FALSE), 3)
+  expected <- round(c(0.1161076, 0.1793267, 0.4967071, 0.1695518, 0.1851988, 0.4993293, 0.3738221, 0.3280985, 0.1285719, 0.2104207), 3)
+  expect_equal(r2, expected)
+})
